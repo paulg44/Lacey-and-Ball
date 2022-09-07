aea863e0d0f056bc21bc11ddabb40eac282a6447
Paul Garton <paulgarton1984@hotmail.com>
2022-09-07 20:23:13 +0100

    Add modal script

---
+++ script1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const showModals = document.getElementsByClassName('show-modal');
+const modalArray = Array.from(showModals).entries();
+const modals = document.getElementsByClassName('modal');
+const closeModal = document.getElementsByClassName('close-modal');
+
+for (let [index, showModals] of modalArray) {
+  const toggleModal = () => {
+    modals[index].classList.toggle('hidden');
+  };
+  showModals.addEventListener('click', toggleModal);
+  closeModal[index].addEventListener('click', toggleModal);
+}
